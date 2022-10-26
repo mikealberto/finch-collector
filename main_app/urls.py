57c00e89e1b6bd96d1509d12569a84a63033648b
mikealberto <alberto.michael1701@gmail.com>
2022-10-26 16:16:02 -0400

    Add home and about path, set views for home and about, base and about templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
-from django.import path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about")
 ]
