f18d8b63ae5270206f8cab0c8f227ea92062c012
mikealberto <alberto.michael1701@gmail.com>
2022-10-27 13:46:00 -0400

    create finch model in main app/models and migrate it, comment out dummy data in views(controller), create home template

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,25 @@
+from http.client import CannotSendHeader
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # dummy data
-class Finch:
-    def __init__(self, name, species, description, age):
-        self.name = name
-        self.species = species
-        self.description = description
-        self.age = age
+##### Delete #####
+# class Finch:
+#     def __init__(self, name, species, description, age):
+#         self.name = name
+#         self.species = species
+#         self.description = description
+#         self.age = age
 
-finches = [
-    Finch("Beak", "House", "Well Studied", 4),
-    Finch("Claw", "Cassin", "Long Head", 7),
-    Finch("Chippy", "Cassia Crossbill", "Doesn't Migrate", 2),
-    Finch("Falcon", "Shaft-tail", "Found in abudance", 0),
-]
-
+# finches = [
+#     Finch("Beak", "House", "Well Studied", 4),
+#     Finch("Claw", "Cassin", "Long Head", 7),
+#     Finch("Chippy", "Cassia Crossbill", "Doesn't Migrate", 2),
+#     Finch("Falcon", "Shaft-tail", "Found in abudance", 0),
+# ]
+###############
 
 def home(request):
-    return HttpResponse("<h1>Hello Finch</h1>")
+    return render(request, "home.html")
 
 def about(request):
     return render(request, "about.html")
